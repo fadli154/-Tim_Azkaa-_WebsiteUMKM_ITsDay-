0a79a86e354e8509f84b234a691c7f18bffe8ceb
fadli154 <fadlihifziansyah154@gmail.com>
2025-06-26 16:00:49 +0700

    add: artikel section

--- src/script/script.js
+++ src/script/script.js
@@ -52,8 +52,71 @@ document.addEventListener("click", (evt) => {
   }
 });
 
+// Glider artikel
+
+// Menyimpan status inisialisasi Glider
+let isGliderInitialized = false;
+
+function checkAndInitGlider() {
+  const draggableElement = document.querySelector(".draggable");
+  const gliderElement = document.querySelector(".artikel-glider");
+
+  if (window.innerWidth > 1024) {
+    // Jika elemen .glider tidak ada dan lebar layar lebih besar dari 1024, inisialisasi Glider
+    if (gliderElement) {
+      const glider = new Glider(gliderElement, {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        draggable: true,
+        rewind: true,
+        dots: ".dots",
+        arrows: {
+          prev: ".glider-prev",
+          next: ".glider-next",
+        },
+      });
+
+      gliderElement._glider = glider; // Store the Glider instance
+      gliderElement.classList.add("glider-initialized");
+      gliderElement.classList.add("glider");
+      isGliderInitialized = true;
+    } else if (draggableElement) {
+      const glider = new Glider(draggableElement, {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        draggable: true,
+        dots: ".dots",
+        arrows: {
+          prev: ".glider-prev",
+          next: ".glider-next",
+        },
+      });
+
+      draggableElement._glider = glider; // Store the Glider instance
+      draggableElement.classList.add("glider-initialized");
+      draggableElement.classList.add("glider");
+      isGliderInitialized = true;
+    }
+  } else if (gliderElement && window.innerWidth < 1024) {
+    // Jika elemen .glider ada dan lebar layar tidak lebih besar dari 1024, hancurkan Glider
+    if (isGliderInitialized) {
+      if (gliderElement._glider) {
+        gliderElement._glider.destroy(); // Destroy the Glider instance
+        delete gliderElement._glider; // Remove the stored Glider instance
+      }
+      gliderElement.classList.remove("glider-initialized");
+      isGliderInitialized = false;
+    }
+  }
+}
+
+// Memanggil fungsi saat halaman dimuat
+window.addEventListener("load", checkAndInitGlider);
+
 // Memanggil fungsi saat lebar layar berubah
 window.addEventListener("resize", function () {
+  checkAndInitGlider();
+
   if (window.innerWidth > 768) {
     if (!blankDisplay.classList.contains("scale-0")) {
       blankDisplay.classList.add("scale-0");
